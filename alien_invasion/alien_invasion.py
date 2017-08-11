eb381d61ca58e81071ffa9afdfd3af9247383d77
Niesha <nieshaamos@gmail.com>
2017-08-11 15:27:56 -0400

    Create ship

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 def run_game():
     #Initialize pygame, setting and screen object
@@ -9,6 +10,9 @@ def run_game():
     ai_settings = Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
+
+    #Make a ship
+    ship = Ship(screen)
 
     #Set the backgroun color
     #bg_color = (230, 230, 230)
@@ -23,6 +27,7 @@ def run_game():
 
         #Redraw the screen during each pass through the loop
         #screen.fill(ai_settings.bg_color)
+        ship.blitme()
 
         #Make the most recently drawn screen visible
         pygame.display.flip()
