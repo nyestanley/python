0ad9a14fb760053fc85e73ca617d9cb631090ac5
Niesha <nieshaamos@gmail.com>
2017-08-18 16:00:33 -0400

    Limit number of bullets

--- alien_invasion/game_functions.py
+++ alien_invasion/game_functions.py
@@ -19,9 +19,10 @@ def check_keydown_events(event, ai_settings, screen, ship, bullets):
         #Move the ship down
         ship.moving_down = True
     elif event.key == pygame.K_SPACE:
-        #Create a new bullet and add it to the bullets group
-        new_bullet = Bullet(ai_settings, screen, ship)
-        bullets.add(new_bullet)
+        # Create a new bullet and add it to the bullets group
+        if len(bullets) < ai_settings.bullets_allowed:
+            new_bullet = Bullet(ai_settings, screen, ship)
+            bullets.add(new_bullet)
 
 def check_keyup_events(event, ship):
     """Respond to key releases."""
