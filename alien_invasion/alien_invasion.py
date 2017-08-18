ee90e0216d6fe6246a8b75106196fdbaee38afe1
Niesha <nieshaamos@gmail.com>
2017-08-18 12:06:07 -0400

    Add ship controls - up, down, left and right

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -1,8 +1,8 @@
-import sys
 import pygame
 
 from settings import Settings
 from ship import Ship
+import game_functions as gf
 
 def run_game():
     #Initialize pygame, setting and screen object
@@ -12,7 +12,7 @@ def run_game():
     pygame.display.set_caption("Alien Invasion")
 
     #Make a ship
-    ship = Ship(screen)
+    ship = Ship(ai_settings, screen)
 
     #Set the backgroun color
     #bg_color = (230, 230, 230)
@@ -21,16 +21,9 @@ def run_game():
     while True:
 
         #Watch for keyboard and mouse events
-        for event in pygame.event.get():
-            if event.type==pygame.QUIT:
-                sys.exit()
-
-        #Redraw the screen during each pass through the loop
-        #screen.fill(ai_settings.bg_color)
-        ship.blitme()
-
-        #Make the most recently drawn screen visible
-        pygame.display.flip()
+        gf.check_events(ship)
+        ship.update()
+        gf.update_screen(ai_settings, screen, ship)
 
 
 
--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -6,4 +6,5 @@ class Settings():
         #Screen settings
         self.screen_width = 1000
         self.screen_height= 700
-        #self.bg_color = (230,230,230)
+        self.bg_color = (0,0,0)
+        self.ship_speed_factor = 1.5
