d25af0d3037ca1f028b99b3972eed99b51790a45
Niesha <nieshaamos@gmail.com>
2017-08-31 14:06:39 -0400

    Add repopulate fleet (when all aliens are shot down)

--- alien_invasion/game_functions.py
+++ alien_invasion/game_functions.py
@@ -64,7 +64,7 @@ def update_screen(ai_settings, screen, ship, aliens, bullets):
     # Make the most recently drawn screen visible
     pygame.display.flip()
 
-def update_bullets(bullets, aliens):
+def update_bullets(ai_settings, screen, ship, aliens, bullets):
     """Update position of bullets and get rid of old bullets"""
     #Update bullet positions
     bullets.update()
@@ -79,6 +79,11 @@ def update_bullets(bullets, aliens):
     #If so, get rid of the bullet and the alien.
     collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
     #Note: to make a high powered bullet, set first Boolean argument to False so it can hit multiple aliens
+
+    if len(aliens)==0:
+        #Destroy existing bullets and create a new fleet.
+        bullets.empty()
+        create_fleet(ai_settings, screen, ship, aliens)
 
 def fire_bullet(ai_settings, screen, ship, bullets):
     """Fire a bullet if limit not reached yet"""
