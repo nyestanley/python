9e3b92f8da0f0d7416a04aeb86e3b5ba368ebe9c
Niesha <nieshaamos@gmail.com>
2017-08-31 13:29:33 -0400

    Add alien fleet

--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -17,3 +17,9 @@ class Settings():
         self.bullet_height = 15
         self.bullet_color = (234,0,195)
         self.bullets_allowed =100
+
+        #Alien settings
+        self.alien_speed_factor = 1
+        self.fleet_drop_speed = 10
+        #fleet_direction of 1 represents right; -1 represents left
+        self.fleet_direction = 1
