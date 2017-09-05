16ad723d95204ee4275803fa74050b1282319edf
Niesha <nieshaamos@gmail.com>
2017-09-04 23:07:03 -0400

    Fix typo on line 207

--- alien_invasion/game_functions.py
+++ alien_invasion/game_functions.py
@@ -162,6 +162,9 @@ def update_aliens(ai_settings, stats, screen, ship, aliens, bullets):
     if pygame.sprite.spritecollideany(ship, aliens):
         ship_hit(ai_settings, stats, screen, ship, aliens, bullets)
 
+    #look for aliens hitting the bottom of the screen.
+        check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets)
+
 def check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets):
     """Respond to bullet-alien collisions."""
     #Check for any bullets that have hit aliens.
@@ -174,9 +177,19 @@ def check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets):
         bullets.empty()
         create_fleet(ai_settings, screen, ship, aliens)
 
+def check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets):
+    """Check if any aliens have reached the bottom of the screen."""
+    screen_rect = screen.get_rect()
+    for alien in aliens.sprites():
+        if alien.rect.bottom >= screen_rect.bottom:
+            #Treat this the same as if ship got hit.
+            ship_hit(ai_settings, stats, screen, ship, aliens, bullets)
+
+            break
+
 def ship_hit(ai_settings, stats, screen, ship, aliens, bullets):
     """Respond to ship being hit by alien"""
-    if stats.ships_left > 0:
+    if stats.ships_left > 1:
         #Decrement ships_left
         stats.ships_left -= 1
 
@@ -191,7 +204,7 @@ def ship_hit(ai_settings, stats, screen, ship, aliens, bullets):
         #Pause
         sleep(1)
     else:
-        stats.game_Active = False
+        stats.game_active = False
         pygame.mouse.set_visible(True)
 
 def check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bullets, mouse_x, mouse_y):
